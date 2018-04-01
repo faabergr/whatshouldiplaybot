9d649616edacc5e2a68b8910b32078071654cad8
Rick Faaberg <rick@faaberg.org>
2018-04-01 14:39:41 -0700

    Error logging

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const bgg = require("bgg")();
+const winston = require("winston");
+
+winston.add(winston.transports.File, { name: "trace", filename: "trace.log" });
+winston.add(winston.transports.File, {
+  name: "errors",
+  filename: "errors.log",
+  level: "error"
+});
 
 getRecommendation(process.argv[2]);
 
@@ -8,46 +16,55 @@ function getRecommendation(bggUsername) {
   bgg("collection", {
     username: bggUsername,
     excludesubtype: "boardgameexpansion"
-  }).then(results => {
-    if (!results.items.item) {
-      console.log(results.item.item);
-    }
-    console.log(`found ${results.items.item.length} items`);
-    const ownedBoardgames = results.items.item.filter(
-      item => item.subtype === "boardgame" && item.status.own === 1
-    );
-    console.log(`of those, ${ownedBoardgames.length} are owned`);
+  })
+    .then(results => {
+      if (!results.items || !results.items.item) {
+        winston.info(`Could not find any results for ${bggUsername}`);
+        return;
+      }
+      winston.info(`found ${results.items.item.length} items`);
+      const ownedBoardgames = results.items.item.filter(
+        item => item.subtype === "boardgame" && item.status.own === 1
+      );
+      winston.info(`of those, ${ownedBoardgames.length} are owned`);
 
-    var gameIndex = getRandomInt(0, ownedBoardgames.length);
-    var pickedGame = ownedBoardgames[gameIndex];
+      var gameIndex = getRandomInt(0, ownedBoardgames.length);
+      var pickedGame = ownedBoardgames[gameIndex];
 
-    bgg("thing", { id: pickedGame.objectid }).then(results => {
-      const item = results.items.item;
-      const playerCounts =
-        item.minplayers.value === item.maxplayers.value
-          ? `For ${item.minplayers.value} players,`
-          : `For ${item.minplayers.value}-${item.maxplayers.value} players,`;
+      bgg("thing", { id: pickedGame.objectid }).then(results => {
+        const item = results.items.item;
+        const playerCounts =
+          item.minplayers.value === item.maxplayers.value
+            ? `For ${item.minplayers.value} players,`
+            : `For ${item.minplayers.value}-${item.maxplayers.value} players,`;
 
-      const timeRange =
-        item.minplaytime.value === item.maxplaytime.value
-          ? `it takes approximately ${item.minplaytime.value} minutes`
-          : `it takes between ${item.minplaytime.value} and ${
-              item.maxplaytime.value
-            } minutes`;
+        const timeRange =
+          item.minplaytime.value === item.maxplaytime.value
+            ? `it takes approximately ${item.minplaytime.value} minutes`
+            : `it takes between ${item.minplaytime.value} and ${
+                item.maxplaytime.value
+              } minutes`;
 
-      const preamble = chooseOne([
-        "you should play",
-        "why don't you try",
-        "I think you should play"
-      ]);
+        const preamble = chooseOne([
+          "you should play",
+          "why don't you try",
+          "I think you should play"
+        ]);
 
-      console.log(
-        `${bggUsername}, ${preamble} ${
-          pickedGame.name.$t
-        }. ${playerCounts} ${timeRange} to play.`
+        winston.info(
+          `${bggUsername}, ${preamble} ${
+            pickedGame.name.$t
+          }. ${playerCounts} ${timeRange} to play.`
+        );
+      });
+    })
+    .catch(result => {
+      winston.error(
+        `Could not connect to BGG API at ${result.url}: ${
+          result.raw.response.statusCode
+        } ${result.raw.response.statusMessage}`
       );
     });
-  });
 }
 
 function chooseOne(choices) {
