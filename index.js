cd34197fb9baabe71b4c639438f477dd4b67490e
faabergr <rick@faaberg.org>
2018-04-08 22:09:36 -0700

    Caching and such

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const minutesPattern = /(\d+)?(min|m|minutes|mins)$/;
 const hoursPattern = /(\d+)(hr|h|hours|hrs|hr)$/;
 const playersPattern = /(\d+)(p|players|people|peeps|player)$/;
 
+const fiveMinutesInMilliseconds = 5 * 60 * 1000;
+
 winston.add(winston.transports.File, { name: "trace", filename: "trace.log" });
 winston.add(winston.transports.File, {
   name: "errors",
@@ -16,27 +18,41 @@ winston.add(winston.transports.File, {
 });
 
 const collectionCache = new Map();
+const itemCache = new Map();
 
 if (process.argv.length > 2) {
   let params = extractParameters(process.argv[2]);
-  winston.info(
+  let requestId = guid();
+  info(
     `extracted game time of ${params.gameTime} minutes and player count of ${
       params.playerCount
+    } and username ${
+      params.bggUsername
     } and too many players: ${params.tooManyPlayerCounts ===
-      true} and too many times: ${params.tooManyTimes === true}`
+      true} and too many times: ${params.tooManyTimes === true}`,
+    requestId
   );
+
+  getRecommendation(params.bggUsername).then(message => {
+    info(message, requestId);
+  });
 }
 
 var stream = T.stream("statuses/filter", { track: ["@wsipbot"] });
 stream.on("tweet", tweetEvent);
 
 function tweetEvent(tweet) {
+  let requestId = guid();
   winston.info(`tweet from ${tweet.user.screen_name}`);
   let params = extractParameters(tweet);
-  winston.info(
+  info(
     `extracted game time of ${params.gameTime} minutes and player count of ${
       params.playerCount
-    }`
+    } and username ${
+      params.bggUsername
+    } and too many players: ${params.tooManyPlayerCounts ===
+      true} and too many times: ${params.tooManyTimes === true}`,
+    requestId
   );
 
   var name = tweet.user.screen_name;
@@ -103,22 +119,28 @@ function extractParameters(tweet) {
   params.playerCount =
     (playerCounts.length == 1 && getPlayerCount(playerCounts[0])) || 0;
 
+  let unclassifiedTerms = splitTweet.filter(
+    term =>
+      !termIsMinutes(term) && !termIsHours(term) && !termIsPlayerCount(term)
+  );
+  if (unclassifiedTerms > 1) return { tooManyBggUsernames: true };
+  if (unclassifiedTerms === 0) return { tooFewBggUsernames: true };
+
+  params.bggUsername = unclassifiedTerms[0];
   return params;
 }
-
-//getRecommendation(process.argv[2]);
 
 function getRecommendation(bggUsername) {
   winston.info(`getting recommendations for ${bggUsername}`);
 
-  getCollection(bggUsername)
+  return getCollection(bggUsername)
     .then(results => {
-      if (!results.items || !results.items.item) {
+      if (!results || results.length === 0) {
         winston.info(`Could not find any results for ${bggUsername}`);
         return;
       }
-      winston.info(`found ${results.items.item.length} items`);
-      const ownedBoardgames = results.items.item.filter(
+      winston.info(`found ${results.length} items`);
+      const ownedBoardgames = results.filter(
         item => item.subtype === "boardgame" && item.status.own === 1
       );
       winston.info(`of those, ${ownedBoardgames.length} are owned`);
@@ -126,8 +148,8 @@ function getRecommendation(bggUsername) {
       var gameIndex = getRandomInt(0, ownedBoardgames.length);
       var pickedGame = ownedBoardgames[gameIndex];
 
-      bgg("thing", { id: pickedGame.objectid }).then(results => {
-        const item = results.items.item;
+      getGame(pickedGame.objectid).then(results => {
+        const item = results;
         const playerCounts =
           item.minplayers.value === item.maxplayers.value
             ? `For ${item.minplayers.value} players,`
@@ -146,11 +168,13 @@ function getRecommendation(bggUsername) {
           "I think you should play"
         ]);
 
-        winston.info(
-          `${bggUsername}, ${preamble} ${
-            pickedGame.name.$t
-          }. ${playerCounts} ${timeRange} to play.`
-        );
+        let message = `${bggUsername}, ${preamble} ${
+          pickedGame.name.$t
+        }. ${playerCounts} ${timeRange} to play.`;
+
+        winston.info(message);
+
+        return Promise.resolve(message);
       });
     })
     .catch(result => {
@@ -168,18 +192,26 @@ function chooseOne(choices) {
 
 function getCollection(username) {
   var collection = collectionCache.get(username);
-  if (!collection) {
+  if (
+    !collection ||
+    collection.expiration + fiveMinutesInMilliseconds <= Date.now()
+  ) {
     return bgg("collection", {
       username: username,
       excludesubtype: "boardgameexpansion"
     }).then(results => {
-      winston.info(
-        `Setting cache for ${username} with ${
-          results.items.item.length
-        } size collection`
-      );
-      collectionCache.set(username, results);
-      return results;
+      if (results.items.item) {
+        winston.info(
+          `Setting cache for ${username} with ${
+            results.items.item.length
+          } size collection`
+        );
+        collectionCache.set(username, {
+          expiration: Date.now(),
+          results: results.items.item
+        });
+      }
+      return Promise.resolve(results.items.item);
     });
   } else {
     winston.info(
@@ -191,8 +223,45 @@ function getCollection(username) {
   }
 }
 
+function getGame(id) {
+  let item = itemCache.get(id);
+  if (!item || item.expiration + fiveMinutesInMilliseconds <= Date.now()) {
+    winston.info(
+      `Cache miss on game ${id}, retrieving from bgg (item was expired: ${(item &&
+        item.expiration + fiveMinutesInMilliseconds <= Date.now()) ||
+        false})`
+    );
+    return bgg("thing", { id: id }).then(results => {
+      if (results.items.item) {
+        winston.info(`Retrieved info on game ${id} from bgg, setting cache`);
+        itemCache.set(id, {
+          expiration: Date.now(),
+          results: results.items.item
+        });
+        return Promise.resolve(results.items.item);
+      }
+    });
+  } else {
+    return item;
+  }
+}
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function info(message, guid) {
+  winston.info(message, { requestId: guid });
+}
+
+function guid() {
+  return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
 }
